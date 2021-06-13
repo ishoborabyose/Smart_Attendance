cb67f2cf39ca2eba29e0162f2bd8a2fc5b645e3a
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2021-06-13 10:20:36 +0200

    fixing heroku

--- src/config/config.js
+++ src/config/config.js
@@ -10,6 +10,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    ssl: true,
     logging: false,
     seederStorage: 'sequelize'
   },
@@ -19,6 +20,7 @@ module.exports = {
     database: process.env.DB_NAME_TEST,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    ssl: true,
     logging: false,
     seederStorage: 'sequelize'
   },
@@ -28,6 +30,7 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    ssl: true,
     logging: false,
     seederStorage: 'sequelize'
   }
